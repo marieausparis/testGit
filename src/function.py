a831e33c0f5ababab9520e6fb176bcb286f9d687
marieausparis <viveelsueno@gmail.com>
2025-11-09 18:11:33 +0100

    First commit overiew

---
+++ src/function.py
@@ -0,0 +1,4 @@
+def get_marie_aus_paris()->str:
+    return "Je suis Marie de Paris"
+
+
---
+++ src/main.py
@@ -0,0 +1,6 @@
+
+from src.function import get_marie_aus_paris
+
+
+if __name__ == "__main__":
+    get_marie_aus_paris()
